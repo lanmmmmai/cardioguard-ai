3de8a1daed48f4dd5a88f2bc84a724fe6f62a2f3
lanmmmmai <161665622+lanmmmmai@users.noreply.github.com>
2026-05-27 08:41:17 +0700

    fix backend url

--- web_frontend/src/config.ts
+++ web_frontend/src/config.ts
@@ -1,2 +1,5 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'https://cardioguard-backend-5yb7.onrender.com';
-export const WS_URL = import.meta.env.VITE_WS_URL || 'wss://cardioguard-backend-5yb7.onrender.com';
+export const API_URL =
+  import.meta.env.VITE_API_URL || 'https://cardioguard-ai-a26e.onrender.com';
+
+export const WS_URL =
+  import.meta.env.VITE_WS_URL || 'wss://cardioguard-ai-a26e.onrender.com/ws/realtime';
